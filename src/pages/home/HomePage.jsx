6e635b98734b2f58c3e09d99bc16a1b75f045e35
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-07-03 14:51:48 +0200

    home page progress big big

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -7,58 +7,41 @@ import CostumButton from "../button/Button";
 export default function HomePage() {
     return (
         <div className="homepage">
+            <div className="carusel"></div>
             <div className="infoBoxes">
                 <div className="box">
                     <img src={Info1} alt="" srcset="" />
-                    <h2>Wir sind für dich da!</h2>
-                    <p>
-                        Hier findest du alle aktuellen Informationen zu den Services
-                        und Öffnungszeiten der McDonald's Restaurants in deiner Region.
-                    </p>
-                    <CostumButton text={"Mehr Erfahren"} />
+                    <div className="textbuttonheader">
+                        <p className="boxheader">Wir sind für dich da!</p>
+                        <p>
+                            Hier findest du alle aktuellen Informationen zu den Services
+                            und Öffnungszeiten der McDonald's Restaurants in deiner Region.
+                        </p>
+                        <CostumButton text={"Mehr Erfahren"} />
+                    </div>
                 </div>
                 <div className="box">
                     <img src={Info2} alt="" srcset="" />
-                    <h2>Unsere Sicherheitsmassnahmen</h2>
-                    <p>
-                        Die Gesundheit und Sicherheit aller Gäste und Mitarbeitenden wird bei uns gross geschrieben
-                    </p>
-                    <CostumButton text={"Mehr Informationen"} />
+                    <div className="textbuttonheader">
+                        <p className="boxheader">Unsere Sicherheitsmassnahmen</p>
+                        <p>
+                            Die Gesundheit und Sicherheit aller Gäste und Mitarbeitenden wird bei uns gross geschrieben
+                        </p>
+                        <CostumButton text={"Mehr Informationen"} />
+                    </div>
                 </div>
                 <div className="box">
                     <img src={Info3} alt="" srcset="" />
-                    <h2>Wir sind für dich da!</h2>
-                    <p>
-                        Hier findest du alle aktuellen Informationen zu den Services
-                        und Öffnungszeiten der McDonald's Restaurants in deiner Region.
-                    </p>
-                    <CostumButton text={"Mehr Erfahren"} />
+                    <div className="textbuttonheader">
+                        <p className="boxheader">Wir sind für dich da!</p>
+                        <p>
+                            Hier findest du alle aktuellen Informationen zu den Services
+                            und Öffnungszeiten der McDonald's Restaurants in deiner Region.
+                        </p>
+                        <CostumButton text={"Mehr Erfahren"} />
+                    </div>
                 </div>
             </div>
         </div>
     )
 }
-
-
-/*
- <div className="carusel"></div>
-            <div className="infoBoxes">
-                <div className="box">
-                    <img src={Info1} alt="" srcset="" />
-                    <h2>Wir sind für dich da!</h2>
-                    <p>
-                        Hier findest du alle aktuellen Informationen zu den Services
-                        und Öffnungszeiten der McDonald's Restaurants in deiner Region.
-                    </p>
-                    <CostumButton text={"Mehr Erfahren"} />
-                </div>
-                <div className="box">
-                    <img src={Info2} alt="" srcset="" />
-                    <h2>Unsere Sicherheitsmassnahmen</h2>
-                    <p>
-                        Die Gesundheit und Sicherheit aller Gäste und Mitarbeitenden wird bei uns gross geschrieben
-                    </p>
-                </div>
-
-                </div>
-*/
