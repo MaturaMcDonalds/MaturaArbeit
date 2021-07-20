0199333309c9a8894cfe34662774c87ff49d0793
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-07-20 18:21:17 +0200

    navbar support

--- src/pages/components/navbar/Navbar.jsx
+++ src/pages/components/navbar/Navbar.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
 import Logo from "../../../assets/Logo.png";
 import { AiOutlineMenu } from "react-icons/ai";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 const Navbar = () => {
+    let location = useLocation();
+    const toggle = () => {
+        document.querySelector(".links").classList.toggle("open");
+        document.querySelectorAll(".links li").forEach(link => {
+            link.classList.toggle("fade");
+        })
+    }
+    React.useEffect(() => {
+        document.querySelector(".links").classList.remove("open");
+        document.querySelectorAll(".links li").forEach(link => {
+            link.classList.remove("fade");
+        })
+    }, [location]);
     return (
         <nav className="navbar">
             <div className="logo">
@@ -38,7 +51,7 @@ const Navbar = () => {
                     </li>
                 </ul>
             </div>
-            <div className="sidebar">
+            <div className="toggle" onClick={toggle}>
                 <AiOutlineMenu size={40} />
             </div>
         </nav>
