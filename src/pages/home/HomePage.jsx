0da616cc9579141e697c211bf25062b71fca828f
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-07-25 20:29:45 +0200

    matura paged started

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -4,42 +4,44 @@ import Info2 from "../../assets/home/Info2.png";
 import Info3 from "../../assets/home/Info3.png";
 import CostumButton from "../components/button/Button";
 
+//<img src="https://github.com/MaturaMcDonalds/MaturaArbeit/blob/master/src/assets/home/Pic2.jpg?raw=true" alt="" className="img" />
 export default function HomePage() {
     return (
         <div className="homepage">
             <div className="carusel">
+
             </div>
             <div className="infoBoxes">
                 <div className="box">
                     <img src={Info1} alt="" srcset="" />
                     <div className="textbuttonheader">
                         <p className="boxheader">Wir sind für dich da!</p>
-                        <p>
+                        <p className="boxtext">
                             Hier findest du alle aktuellen Informationen zu den Services
                             und Öffnungszeiten der McDonald's Restaurants in deiner Region.
                         </p>
-                        <CostumButton text={"Mehr Erfahren"} />
+                        <CostumButton text={"Mehr Erfahren"} className="boxbtn" />
                     </div>
                 </div>
                 <div className="box">
                     <img src={Info2} alt="" srcset="" />
                     <div className="textbuttonheader">
                         <p className="boxheader">Unsere Sicherheitsmassnahmen</p>
-                        <p>
+                        <p className="boxtext">
                             Die Gesundheit und Sicherheit aller Gäste und Mitarbeitenden wird bei uns gross geschrieben
                         </p>
-                        <CostumButton text={"Mehr Informationen"} />
+                        <CostumButton text={"Mehr Informationen"} className="boxbtn" />
                     </div>
                 </div>
                 <div className="box">
                     <img src={Info3} alt="" srcset="" />
                     <div className="textbuttonheader">
                         <p className="boxheader">Wir sind für dich da!</p>
-                        <p>
+                        <p className="boxtext">
                             Hier findest du alle aktuellen Informationen zu den Services
                             und Öffnungszeiten der McDonald's Restaurants in deiner Region.
                         </p>
-                        <CostumButton text={"Mehr Erfahren"} />
+                        <CostumButton text={"Mehr Erfahren"} className="boxbtn" />
                     </div>
                 </div>
             </div>
--- src/pages/matura/Matura.jsx
+++ src/pages/matura/Matura.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 export default function Matura() {
     return (
         <div className="matura">
-
+            <section>
+                <h1></h1>
+            </section>
         </div>
     )
 }
