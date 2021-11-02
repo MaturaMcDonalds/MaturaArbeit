8f0869ee11a2b65aebf8a6841c44ed37b325931c
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-11-02 22:46:41 +0100

    overview page

--- src/pages/geschichte/Geschichte.jsx
+++ src/pages/geschichte/Geschichte.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
-import CostumButton from "../components/button/Button";
-import pic1 from "../../assets/img/grundprinzipien1.png";
 import { useHistory } from "react-router-dom";
+
+import pic1 from "../../assets/img/grundprinzipien1.png";
+import box1 from "../../assets/img/box1.png"
+import box2 from "../../assets/img/box2.png"
+import box3 from "../../assets/img/box3.png"
 
 const Geschichte = () => {
     let history = useHistory();
@@ -13,22 +16,50 @@ const Geschichte = () => {
                 <div className="textOverImg">
                     <h2>Grundprinzipien und wo sie herkommen</h2>
                     <p>Eine geschichtliche Einführungs in das Universum des riesigen Fast-Food Konzerns</p>
-                    <CostumButton text={"Mehr Erfahren"} className="boxbtn" action={()=>{
-                        history.push("/MaturaArbeit/Grundprinzipien")
-                    }}/>
+                    <button onClick={()=>{history.push("/MaturaArbeit/Grundprinzipien")}}><p>Mehr erfahren</p></button>
                 </div>
             </div>
             <div className="articleBox">
-                <div className="left"></div>
-                <div className="right"></div>
+                <div className="left">
+                    <h1>
+                    Die McDonaldisierung der Welt
+                    </h1>
+                    <p className="boxtext">
+                    Der American Way of Life jetzt endlich national und international überall verfügbar
+                    </p>
+                    <button><p>Nachlesen</p></button>
+                </div>
+                <div className="right">
+                    <img src={box1} alt="" />
+                </div>
             </div>
             <div className="articleBox">
-                <div className="left"></div>
-                <div className="right"></div>
+                <div className="left">
+                    <h1>
+                    Manipulierte McDonald’s Kinder?
+                    </h1>
+                    <p className="boxtext">
+                    Das Problem, welches eine junge Zielgruppe mit sich bringt
+                    </p>
+                    <button><p>Nachlesen</p></button>
+                </div>
+                <div className="right">
+                <img src={box2} alt="" />
+                </div>
             </div>
             <div className="articleBox">
-                <div className="left"></div>
-                <div className="right"></div>
+                <div className="left">
+                    <h1>
+                    Super-Size-Me und dessen Auswirkungen
+                    </h1>
+                    <p className="boxtext">
+                    Wie ein Amateurfilm zu einem transparenteren und gesünderen McDonald’s verhalf
+                    </p>
+                    <button><p>Nachlesen</p></button>
+                </div>
+                <div className="right">
+                <img src={box3} alt="" />
+                </div>
             </div>
         </div>
     );
