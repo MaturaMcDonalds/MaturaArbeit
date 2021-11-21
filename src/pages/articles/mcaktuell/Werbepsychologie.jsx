66e41a602c4f2512814c259cac7803bbe53688dc
AndreiRobertoOleniuc <andreioleniucroberto@gmail.com>
2021-11-21 23:08:58 +0100

    mail articles done

--- src/pages/articles/mcaktuell/Werbepsychologie.jsx
+++ src/pages/articles/mcaktuell/Werbepsychologie.jsx
@@ -1,10 +1,130 @@
 import React from 'react';
+import pic1 from "../../../assets/articles/mcaktuell/7.png";
+import pic2 from "../../../assets/articles/mcaktuell/8.png";
+import pic3 from "../../../assets/articles/mcaktuell/9.png";
+import pic4 from "../../../assets/articles/mcaktuell/10.png";
+import pic5 from "../../../assets/articles/mcaktuell/11.png";
+import pic6 from "../../../assets/articles/mcaktuell/12.png";
+import pic7 from "../../../assets/articles/mcaktuell/13.png";
 
 const Werbepsychologie = () => {
     return (
-        <div>
-            
-        </div>
+        <article className="article">
+            <h1 className="title">Werbepsychologie</h1>
+            <br />
+            <img src={pic1} alt="" className="hero"/>
+            <br />
+            <br />
+            <p>In diesem Artikel wird die Werbepsychologie des Fast-Food-Giganten McDonald’s genauer beleuchtet. In einem ersten Schritt werden Begriffe definiert und danach ein grundlegendes Modell verständlich erklärt. £in einem weiteren Schritt wird kurz erläutert, was in unserem Körper passiert, wenn wird Werbung wahrnehmen. Die Frage, weshalb McDonald’s seine Kinder als seine gescheit gewählte Zielgruppe definiert hat, wird von einem werbepsychologischen Standpunkt geklärt. Zu guter Letzt werden einige Werbebeispiele des Riesenkonzerns aufgeführt und kurz analysiert. Wir wollen durch diesen Artikel Dein Verständnis der Werbepsychologie vergrössern. Dieses Verständnis ist wichtig, da hinter dem Marketing von McDonald’s ein ausgeklügeltes System steckt, welches ebenfalls die Werbepsychologie beinhaltet. Wie Du, mussten wir uns dieses Wissen zuerst aneignen und durften hierfür ein sehr interessantes Gespräch mit Herrn Werner Schaeppi – einem Werbepsychologen  – führen. 
+            </p>
+            <br />
+            <br />
+            <p className="paragraphovertitle">
+            Begriffsdefinition: Werbepsychologie
+            </p>
+            <br />
+            <p>
+            Doch was versteht man überhaupt unter dem Begriff der Werbepsychologie? Die Werbepsychologie befasst sich mit den Bedingungen und Konsequenzen der Kommunikationsstrategien. Unter Kommunikationsstrategie kann man die Art und Weise verstehen, wie McDonald’s ihre Ziele der Kommunikation erreichen wollen. McDonald’s will mit jeder Werbung und jeder Botschaft dahinter ein Ziel erreichen. Sei dies nun beispielsweise, dass sie umweltfreundlicher wirken oder aktuelle Trends verfolgen (Kommunikationsziel). Dann legen sie ihre Kommunikation durch Strategien so aus, dass sie dieses Ziel erreichen. Sie analysieren hierfür die formale Gestaltung der Kommunikation oder das Verhalten ihrer Konsumierenden und stellen somit möglichst erfolgreiche Strategien auf. Diesen Prozess kann man dann der Werbepsychologie zuordnen (Werbepsychologie, 2018). 
+            </p>
+            <div className="text-picright">
+            <img src={pic2} alt=""/>
+            <p className="leftmargin">
+            In dieser Abbildung sieht man einige Aspekte der effektvollen Werbemassnahmen, welche auch für McDonald’s wichtig sind. Die meisten davon sind selbsterklärend, nur wirst Du wahrscheinlich das KISS-Prinzip nichtkennen. Dieses wird in unserem wissenschaftlichen Kommentar aufgeführt. 
+            In der Werbepsychologie – bessergesagt in der allgemeinen Psychologie – gibt es Modelle und Prinzipien. Unter Modellen kann man sich ein vereinfachtes Abbild der Theorien vorstellen. Wir werden Dir in diesem Artikel das AIDA-Modell vorstellen, um Dir ein Beispiel aufzuführen. Meistens ist ein Modell in der Psychologie in verschiedene Stufen aufgeteilt, welche den Prozess vereinfacht darstellen. 
+            Unter Prinzipien versteht man Gesetzmässigkeiten, welche im Zusammenhang mit der gewählten Theorie zutreffen. 
+            </p>
+            </div>
+            <br />
+            <br />
+            <p className="paragraphovertitle boldbigger">
+            Das AIDA-Modell
+            </p>
+            <br />
+            <img src={pic3} alt="" className="hero"/>
+            <br />
+            <p>
+            Wie man oben ablesen kann, steht AIDA für Aufmerksamkeit, Interesse, Verlangen (Desire) und Aktion. Dieses Modell ist eines der bekanntesten im Zusammenhang mit der Werbepsychologie. Es soll zeigen, wie Unternehmen vorgehen sollen, um möglichst erfolgreich bei ihrer (potentiellen) Kundschaft anzukommen. In der ersten Stufe soll die Aufmerksamkeit generiert werden. Danach sollen sie das Interesse ihrer Kundschaft anregen. In einem weiteren Schritt sollen sie ein Verlangen kreieren und zuletzt soll für die Kundschaft aufgrund aller vorherigen Stufen nichts Weiteres überbleiben als eine Handlung (beispielsweise eine Kaufentscheidung) zu tätigen. Somit hat das Unternehmen einen vielversprechenden psychologischen Vorgang, welcher je nach Ziel des Unternehmens unterschiedlich ausfallen kann, bei seiner Kundschaft ausgelöst (Das AIDA-Modell – die Werbewirkungsformel verständlich erklärt › SmartMarketingBreaks.eu, 2019). 
+            </p>
+            <br />
+            <p>Dies ist jedoch nur eines etlicher Modelle. Doch laut Werner Schaeppi (2021) gleichen sich diese viel mehr als man zuerst denkt. Sie basieren zum grössten Teil auch aufeinander und werden immer weiterentwickelt.  
+            </p>
+            <br />
+            <br />
+            <br />
+            <p className="paragraphovertitle">
+            Der innere Prozess
+            </p>
+            <br />
+            <p>
+            Ein Faktor, in welchem sich jede Art der Werbung gleicht, ist die darin enthaltende Botschaft. Diese Botschaft wird dann von der (potentiellen) Kundschaft verarbeitet. Diese testet die Glaubwürdigkeit der Botschaft. Sie bilden ebenfalls eine Meinung, welche jedoch nur bedingt von Wichtigkeit ist, da die Meinung alleine das effektive Verhalten der Kund:innen nicht direkt beeinflusst. Die innere Einstellung der Kundschaft muss geändert werden, aber muss schlussendlich eine effektive Handlung ausgelöst werden. Hierzu benutzen Grosskonzerne clevere Tricks, beispielsweise wollen sie die bewusste Auffassung und Verarbeitung der Informationen ihrer Werbung meiden. Sie wollen nicht das ihrer Kundschaft bewusst wird, dass sie in jenem Moment umworben werden. Ebenfalls gibt es eine Art des Lernens, welche unbewusst geschieht. Das Gehirn verarbeitet Informationen passiv. Dies nennt man implizites Lernen. Wenn etwas passiv geschieht, dann ist die logische Denkfähigkeit der Kundschaft nicht gegeben, welche sie im Normalfall von einer Kaufentscheidung abhalten. Das Werbeplakat funktioniert in diesem Zusammenhang der Unbewusstheit, laut Schaeppi (2021), besonders gut, da es passiv wahrgenommen wird. 
+            </p>
+            <br />
+            <p>
+            Ebenso probiert McDonald’s mit einprägsamen Slogans oder Jingles in das Langzeitgedächtnis ihrer Kundschaft zu gelangen. Das Ziel dahinter ist recht deutlich, sie wollen ihre Bekanntheit steigern, beibehalten und sichern. 
+            </p>
+            <br />
+            <p>Auch hat sich herausgestellt, das emotionale Werbung erfolgreicher ist, da Menschen ihr Verhalten auf emotionale Reaktionen nicht steuern können. Diesen Aspekt benutzt McDonald’s ebenso aus (Werbe- Und Konsumentenpsychologie, 2015; Werbung Und Psychologie: So Manipulieren Marken Unser Unterbewusstsein, 2019).  
+            </p>
+            <br />
+            <br />
+            <p className="paragraphovertitle">
+            Zielgruppe
+            </p>
+            <br />
+            <p>
+            Erkennst Du nun, weshalb die Kritik aufgrund McDonald’s gewählten Hauptzielgruppe der Kinder begründet ist?
+            <br />  
+            <br />  
+            Kinder reagieren noch mehr auf emotionale Werbung. Sie haben, Schaeppi (2021) zufolge, auch weniger Filter zur Wahrnehmung der Werbung eingebaut. Somit ist es einfacher ein Kind zu umwerben, da es den Prozess erst viel zu spät oder sogar nicht merkt.   
+            </p>
+            <br />
+            <p>
+            Ein weiterer Faktor ist, dass Kinder weniger Vorurteile gegen McDonald’s haben. Sie hinterfragen die ungesunde Komponente zum Beispiel nicht oder auch die Effekte auf die Umwelt nicht. Durch die angepasste kinderfreundliche Werbebotschaft soll ein Verlangen beim Kind ausgelöst werden. Es wird somit seine Eltern davon überzeugen wollen in den McDonald’s zu gehen. Dies ist auch ein Pluspunkt für den Fast-Food-Giganten, da er sozusagen zwei Fliegen mit einer Klatsche erwischt hat. 
+            </p>
+            <br />
+            <p>
+            McDonald’s umwirbt verstärkt Kinder, in dem sie die Einrichtung farbenfroh und kinderfreundlich gestalten. Was dabei nicht fehlen darf, sind die Spielplätze oder die Spielzeugangebote. So soll das Verlangen der Kinder in den McDonald’s gehen zu wollen verstärkt werden. 
+            </p>
+            <br />
+            <br />
+            <p className="paragraphovertitle">
+            Identität (McDonald’s als Marke und Wahrzeichen) 
+            </p>
+            <br />
+            <p>Die Markenidentität von McDonald’s spielt in dem Ganzen ebenfalls eine sehr wichtige Rolle. Von einem psychologischen Standpunkt aus kann man sagen, dass sobald wir nur schon den Namen des Fast-Food-Giganten hören, diesen mit Erwartungen und bereits vergangenen Erlebnissen verbinden. Auch durch Symbolen, wie beispielsweise den Golden Arches, welche man schon von der Autobahn aus sehen kann, erkennt man McDonald’s überall auf der Welt sofort. 
+            </p>
+            <br />
+            <p>Schaeppi (2021) meint, dass McDonald’s sozusagen als Prototyp des Fast-Food-Restaurant gilt. Dies hilft dem Grosskonzern natürlich enorm, da der Begriff McDonald’s in jeder Munde bekannt ist. Auch kann man deshalb davon ausgehen, dass sie mit ihrer Werbung sich vollkommen auf die Verbesserung und Anpassung ihres Image achten können. Andere Unternehmen müssen zuerst einmal überhaupt ihre (potentielle) Kundschaft über sie aufmerksam machen.  
+            </p>
+            <br />
+            <br />
+            <p className="paragraphovertitle boldbigger">
+            Werbebeispiele McDonald’s 
+            </p>
+            <br />
+            <p>Wie vorhin bereits erwähnt nutzt McDonald’s des Öfteren einprägsame Jingles, wie beispielsweise «I’m loving it», welcher weltweit genutzt wurde.
+            </p>
+            <br />
+            <img src={pic4} alt="" className="hero"/>
+            <br />
+            <br />
+            <div className="text-picright">
+            <img src={pic5} alt=""/>
+            <p className="leftmargin">
+            Redewendungen beispielsweise werden angepasst auf die lokale Umgebung, sodass sich Leute eher verbunden fühlen mit dem unternehmen und zu Kosnsument:innen werden
+            </p>
+            </div>
+            <div className="text-picright">
+            <p>
+            Zusätzlich können sie anhand ihrer Werbung ihr Image aufbessern und sich selbst besser in in der modernen Zeit platzieren. Hier werben sie zum Beispiel für ihren neuen veganen Burger, welcher deutlich den Trend des Veganismus verfolgt.             </p>
+            <img src={pic6} alt=""/>
+            </div>
+            <div className="text-picright">
+            <img src={pic7} alt=""/>
+            <p className="leftmargin">
+            Auch beziehen sie sich mit ihrer Werbung der lokalen Angeboten gerne auf die örtliche Umgebung.  Die Kundschaft soll sich dabei denken: «Wow, diesen Burger gibt es nur hier!», so wird ein Verlangen erschaffen. Das Produkt wird eine Besonderheit im sonst weltweit gleichen Sortiment. Sie schreiben das sogar auf das Werbeplakat, somit muss die Kundschaft nicht einmal selbst daran denken. Dies ist Teil der sogenannten Glokaliserung, in welcher es darum geht lokal sich anzupassen aber global zu denken.            </p>
+            </div>
+        </article>
     );
 }
 
